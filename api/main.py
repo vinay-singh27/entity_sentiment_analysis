0d77a86b7b434b08d08a63ccec293fe424caa159
vinay <vinaysin27@gmail.com>
2023-08-19 17:09:23 +0530

    streamlit branch

--- api/main.py
+++
@@ -1,8 +0,0 @@
-import api
-import documentation
-import justpy as jp
-
-
-jp.Route("/api", api.API.serve)
-jp.Route("/", documentation.Doc.serve)
-jp.justpy(port=8000)
--- webapp/page.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class Page(ABC):
-
-    @abstractmethod
-    def serve(self):
-        pass
